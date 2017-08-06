a1f0b37659d055f16f4845c357f11056bb3330cc
TheTrain2000 <gibbstyler7@gmail.com>
2017-08-05 20:18:46 -0500

    enhance botto

--- main.py
+++ main.py
@@ -4,8 +4,13 @@ client = discord.Client()
 
 token = input('gib token')
 
+rude_people = [122122926760656896, 103675685343612928] # devon and borked
+
 @client.event
 async def on_message(message):
     await message.channel.send(f'fuck you {message.author.mention}')
+
+    if message.author.id in rude_people:
+        await message.channel.send('double fuck you')
 
 client.run(token)
